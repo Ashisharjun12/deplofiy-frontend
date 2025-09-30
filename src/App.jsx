1819ae76a6c11d72c480a9a6c026d2e69b846c97
mirajhasanmd <mirajhasanmd6@gmail.com>
2025-09-30 13:10:50 +0530

    frontend-sacdcn based setu template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { ModeToggle } from './components/ui/mode-toggle'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
+  )
+}
+
+export default App
